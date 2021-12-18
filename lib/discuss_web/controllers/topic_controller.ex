15c3355d3385d8053d8e33db91969a843a053fd4
Mateus Costa <info.mateuscosta@gmail.com>
2021-12-17 23:11:20 -0300

    added add and listing pages

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,13 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
+  alias Discuss.Repo
   alias Discuss.Topic
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{})
@@ -9,6 +15,16 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    IO.inspect(topic)
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, _post} ->
+        conn
+        |> put_flash(:info, "Topic created!")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
